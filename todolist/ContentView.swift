3cf1e1e2db4612d3cfef6ab55bb58e33e66a57cd
Aanya <aanyajain712@gmail.com>
2025-07-28 11:57:15 -0500

    Initial Commit

---
+++ todolist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todolist
+//
+//  Created by Aanya Jain on 7/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Aanya Jain on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
